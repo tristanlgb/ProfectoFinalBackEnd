1f57325f3c4c54eeedadd7d2e5aa9e055d625c3c
Tristan Lenz <112999910+tristanlgb@users.noreply.github.com>
2026-05-28 15:15:01 -0300

    Update auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,9 @@
+// src/auth/auth.module.ts
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -11,19 +13,21 @@ import { LocalStrategy } from './local.strategy';
 @Module({
   imports: [
     ConfigModule,
-    PassportModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     UsersModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
+      inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET') || 'secretTemporal',
-        signOptions: { expiresIn: '1h' },
+        signOptions: {
+          expiresIn: '1h',
+        },
       }),
-      inject: [ConfigService],
     }),
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy, LocalStrategy],
-  exports: [AuthService, JwtModule],
+  exports: [AuthService, JwtModule, PassportModule],
 })
 export class AuthModule {}
