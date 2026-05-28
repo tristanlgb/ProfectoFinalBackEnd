5149c6bdca3edb57df7b1366d50b14e06d364446
Tristan Lenz <112999910+tristanlgb@users.noreply.github.com>
2026-05-28 15:29:25 -0300

    Update products.service.ts

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,42 +1,80 @@
 // src/products/products.service.ts
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
+
 import { Product, ProductDocument } from './schemas/product.schema';
-import { Model } from 'mongoose';
-import { NotFoundException } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
 export class ProductsService {
-  constructor(@InjectModel(Product.name) private productModel: Model<ProductDocument>) {}
+  constructor(
+    @InjectModel(Product.name)
+    private readonly productModel: Model<ProductDocument>,
+  ) {}
 
   async findAll(): Promise<Product[]> {
     return this.productModel.find().exec();
   }
 
-  async findOne(id: string): Promise<Product> {
-    return this.productModel.findById(id).exec();
+  async findOne(id: string): Promise<ProductDocument> {
+    this.validateObjectId(id);
+
+    const product = await this.productModel.findById(id).exec();
+
+    if (!product) {
+      throw new NotFoundException(`Product with id ${id} does not exist`);
+    }
+
+    return product;
   }
 
-  async create(createProductDto: any): Promise<Product> {
+  async create(createProductDto: CreateProductDto): Promise<ProductDocument> {
     const createdProduct = new this.productModel(createProductDto);
     return createdProduct.save();
   }
 
-  async update(id: string, updateProductDto: any): Promise<Product> {
-    return this.productModel.findByIdAndUpdate(id, updateProductDto, { new: true }).exec();
+  async update(
+    id: string,
+    updateProductDto: UpdateProductDto,
+  ): Promise<ProductDocument> {
+    this.validateObjectId(id);
+
+    const updatedProduct = await this.productModel
+      .findByIdAndUpdate(id, updateProductDto, { new: true })
+      .exec();
+
+    if (!updatedProduct) {
+      throw new NotFoundException(`Product with id ${id} does not exist`);
+    }
+
+    return updatedProduct;
   }
 
-  async delete(id: string): Promise<any> {
-    return this.productModel.findByIdAndDelete (id).exec();
+  async delete(id: string): Promise<ProductDocument> {
+    this.validateObjectId(id);
+
+    const deletedProduct = await this.productModel.findByIdAndDelete(id).exec();
+
+    if (!deletedProduct) {
+      throw new NotFoundException(`Product with id ${id} does not exist`);
+    }
+
+    return deletedProduct;
   }
 
   async getProductById(id: string): Promise<ProductDocument> {
-    const product = await this.productModel.findById(id).exec();
-    if (!product) {
-      throw new NotFoundException(`Product with id ${id} does not exist`);
-    }
-    return product;
+    return this.findOne(id);
   }
 
-
+  private validateObjectId(id: string): void {
+    if (!Types.ObjectId.isValid(id)) {
+      throw new BadRequestException(`Invalid product id: ${id}`);
+    }
+  }
 }
