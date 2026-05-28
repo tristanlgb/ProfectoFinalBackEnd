5818f38dd9fa88d0c61cfb3bbbafdacb9fe3f648
Tristan Lenz <112999910+tristanlgb@users.noreply.github.com>
2026-05-28 15:23:00 -0300

    Update users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -9,5 +9,6 @@ import { User, UserSchema } from './schemas/user.schema';
   imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
   providers: [UsersService],
   controllers: [UsersController],
+   exports: [UsersService],
 })
 export class UsersModule {}
